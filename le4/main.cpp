196c3566c8a5aec63c862159c6712261a636bf36
Tony Kostanjsek <708330+lobotony@users.noreply.github.com>
2019-01-19 15:29:23 +0100

    embedded SDL2, added some test code

---
+++ le4/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdocumentation"
+#include <SDL2/SDL.h>
+#pragma clang diagnostic pop
+
+using std::cout;
+int main(int argc, const char * argv[])
+{
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+    {
+        cout << "Failed to initialise SDL\n";
+        return -1;
+    }
+    cout << "Hooray it works!\n";
+    SDL_Quit();
+    return 0;
+}
