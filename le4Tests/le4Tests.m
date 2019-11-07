6ba820d547bac73f1123e348a204384ee00d9478
Tony Kostanjsek <708330+lobotony@users.noreply.github.com>
2019-11-07 23:44:56 +0100

    add test target, disable host app for tests

---
+++ le4Tests/le4Tests.m
@@ -0,0 +1,37 @@
+//
+//  le4Tests.m
+//  le4Tests
+//
+//  Created by Anton Kostanjsek on 07.11.19.
+//  Copyright © 2019 Tony Kostanjsek. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface le4Tests : XCTestCase
+
+@end
+
+@implementation le4Tests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
