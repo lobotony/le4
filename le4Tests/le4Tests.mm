4d95f79cd76efd26b3ad91171de0916c9f413c63
Tony Kostanjsek <708330+lobotony@users.noreply.github.com>
2019-11-07 23:51:03 +0100

    got a working test setup

--- le4Tests/le4Tests.mm
+++ le4Tests/le4Tests.mm
@@ -1,12 +1,7 @@
-//
-//  le4Tests.m
-//  le4Tests
-//
-//  Created by Anton Kostanjsek on 07.11.19.
-//  Copyright © 2019 Tony Kostanjsek. All rights reserved.
-//
-
 #import <XCTest/XCTest.h>
+#import "le4.h"
+
+using namespace le4;
 
 @interface le4Tests : XCTestCase
 
@@ -22,16 +17,13 @@
     // Put teardown code here. This method is called after the invocation of each test method in the class.
 }
 
-- (void)testExample {
+- (void)testVec2 {
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
-}
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+    vec2 v(1,2);
+    XCTAssert(v.x == 1);
+    XCTAssert(v.y == 2);
 }
 
 @end
